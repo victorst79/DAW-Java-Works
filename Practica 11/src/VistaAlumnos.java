4c98b57c4de938827e26c368ec99886748a244cf
Victor Santaella <victorst79@gmail.com>
2018-05-04 20:36:35 +0200

    Practica 11

--- Practica 11/src/VistaAlumnos.java
+++ Practica 11/src/VistaAlumnos.java
@@ -27,23 +27,205 @@ public class VistaAlumnos extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jScrollPane3 = new javax.swing.JScrollPane();
+        jTable1 = new javax.swing.JTable();
+        jLabel1 = new javax.swing.JLabel();
+        jSeparator1 = new javax.swing.JSeparator();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        cCurso = new java.awt.Choice();
+        cEvaluacion = new java.awt.Choice();
+        cAsignatura = new java.awt.Choice();
+        bAplicar = new javax.swing.JButton();
+        jLabel5 = new javax.swing.JLabel();
+        jSeparator2 = new javax.swing.JSeparator();
+        jLabel6 = new javax.swing.JLabel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        lCandidatos = new javax.swing.JList();
+        b1 = new javax.swing.JButton();
+        b2 = new javax.swing.JButton();
+        b3 = new javax.swing.JButton();
+        b4 = new javax.swing.JButton();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        lSeleccionados = new javax.swing.JList();
+        jLabel7 = new javax.swing.JLabel();
+        bEvaluar = new javax.swing.JButton();
+        jLabel8 = new javax.swing.JLabel();
+        jSeparator3 = new javax.swing.JSeparator();
+        jScrollPane4 = new javax.swing.JScrollPane();
+        tEvaluar = new javax.swing.JTable();
+        bProcesar = new javax.swing.JButton();
+        bSalir = new javax.swing.JButton();
+
+        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Title 1", "Title 2", "Title 3", "Title 4"
+            }
+        ));
+        jScrollPane3.setViewportView(jTable1);
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Evaluacion de Alumnos");
         setMinimumSize(new java.awt.Dimension(1000, 700));
+        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 1000, Short.MAX_VALUE)
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 700, Short.MAX_VALUE)
-        );
+        jLabel1.setFont(new java.awt.Font("Dialog", 1, 14)); // NOI18N
+        jLabel1.setText("Seleccion Asignatura");
+        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 20, -1, -1));
+        getContentPane().add(jSeparator1, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 50, 860, 20));
+
+        jLabel2.setText("Curso :");
+        getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(170, 70, -1, -1));
+
+        jLabel3.setText("Evaluacion :");
+        getContentPane().add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(140, 110, -1, -1));
+
+        jLabel4.setText("Asignatura :");
+        getContentPane().add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(520, 70, -1, -1));
+        getContentPane().add(cCurso, new org.netbeans.lib.awtextra.AbsoluteConstraints(270, 70, 140, -1));
+        getContentPane().add(cEvaluacion, new org.netbeans.lib.awtextra.AbsoluteConstraints(270, 110, 140, -1));
+        getContentPane().add(cAsignatura, new org.netbeans.lib.awtextra.AbsoluteConstraints(610, 70, 150, -1));
+
+        bAplicar.setText("Aplicar");
+        bAplicar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                bAplicarActionPerformed(evt);
+            }
+        });
+        getContentPane().add(bAplicar, new org.netbeans.lib.awtextra.AbsoluteConstraints(620, 110, 120, -1));
+
+        jLabel5.setFont(new java.awt.Font("Dialog", 1, 14)); // NOI18N
+        jLabel5.setText("Seleccion Alumnos");
+        getContentPane().add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 160, -1, -1));
+        getContentPane().add(jSeparator2, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 200, 850, 10));
+
+        jLabel6.setText("Alumnos Candidatos");
+        getContentPane().add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 230, -1, -1));
+
+        lCandidatos.setModel(new javax.swing.AbstractListModel() {
+            String[] strings = { "Item 1", "Item 2", "Item 3", "Item 4", "Item 5" };
+            public int getSize() { return strings.length; }
+            public Object getElementAt(int i) { return strings[i]; }
+        });
+        jScrollPane1.setViewportView(lCandidatos);
+
+        getContentPane().add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 260, 320, 190));
+
+        b1.setText("> >");
+        getContentPane().add(b1, new org.netbeans.lib.awtextra.AbsoluteConstraints(440, 280, 80, -1));
+
+        b2.setText("> |");
+        b2.setMaximumSize(new java.awt.Dimension(40, 30));
+        b2.setMinimumSize(new java.awt.Dimension(40, 30));
+        b2.setPreferredSize(new java.awt.Dimension(40, 30));
+        b2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                b2ActionPerformed(evt);
+            }
+        });
+        getContentPane().add(b2, new org.netbeans.lib.awtextra.AbsoluteConstraints(440, 320, 80, -1));
+
+        b3.setText("< <");
+        b3.setMinimumSize(new java.awt.Dimension(40, 30));
+        b3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                b3ActionPerformed(evt);
+            }
+        });
+        getContentPane().add(b3, new org.netbeans.lib.awtextra.AbsoluteConstraints(440, 360, 80, -1));
+
+        b4.setText("| <");
+        b4.setMinimumSize(new java.awt.Dimension(40, 30));
+        b4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                b4ActionPerformed(evt);
+            }
+        });
+        getContentPane().add(b4, new org.netbeans.lib.awtextra.AbsoluteConstraints(440, 400, 80, -1));
+
+        lSeleccionados.setModel(new javax.swing.AbstractListModel() {
+            String[] strings = { "Item 1", "Item 2", "Item 3", "Item 4", "Item 5" };
+            public int getSize() { return strings.length; }
+            public Object getElementAt(int i) { return strings[i]; }
+        });
+        jScrollPane2.setViewportView(lSeleccionados);
+
+        getContentPane().add(jScrollPane2, new org.netbeans.lib.awtextra.AbsoluteConstraints(550, 260, 320, 190));
+
+        jLabel7.setText("Alumnos Seleccionados");
+        getContentPane().add(jLabel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(550, 230, -1, -1));
+
+        bEvaluar.setText("Evaluar");
+        bEvaluar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                bEvaluarActionPerformed(evt);
+            }
+        });
+        getContentPane().add(bEvaluar, new org.netbeans.lib.awtextra.AbsoluteConstraints(790, 460, -1, -1));
+
+        jLabel8.setText("Evaluacion Alumnos");
+        getContentPane().add(jLabel8, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 500, -1, -1));
+        getContentPane().add(jSeparator3, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 520, 840, 10));
+
+        tEvaluar.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Title 1", "Title 2", "Title 3", "Title 4"
+            }
+        ));
+        jScrollPane4.setViewportView(tEvaluar);
+
+        getContentPane().add(jScrollPane4, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 530, 620, 120));
+
+        bProcesar.setText("Procesar");
+        getContentPane().add(bProcesar, new org.netbeans.lib.awtextra.AbsoluteConstraints(760, 550, 90, -1));
+
+        bSalir.setText("Salir");
+        bSalir.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                salirApp(evt);
+            }
+        });
+        getContentPane().add(bSalir, new org.netbeans.lib.awtextra.AbsoluteConstraints(760, 600, 90, -1));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void bAplicarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bAplicarActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_bAplicarActionPerformed
+
+    private void b4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_b4ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_b4ActionPerformed
+
+    private void bEvaluarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bEvaluarActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_bEvaluarActionPerformed
+
+    private void b3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_b3ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_b3ActionPerformed
+
+    private void b2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_b2ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_b2ActionPerformed
+
+    private void salirApp(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_salirApp
+        System.exit(0);
+    }//GEN-LAST:event_salirApp
 
     /**
      * @param args the command line arguments
@@ -81,5 +263,35 @@ public class VistaAlumnos extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton b1;
+    private javax.swing.JButton b2;
+    private javax.swing.JButton b3;
+    private javax.swing.JButton b4;
+    private javax.swing.JButton bAplicar;
+    private javax.swing.JButton bEvaluar;
+    private javax.swing.JButton bProcesar;
+    private javax.swing.JButton bSalir;
+    private java.awt.Choice cAsignatura;
+    private java.awt.Choice cCurso;
+    private java.awt.Choice cEvaluacion;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
+    private javax.swing.JLabel jLabel8;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JScrollPane jScrollPane3;
+    private javax.swing.JScrollPane jScrollPane4;
+    private javax.swing.JSeparator jSeparator1;
+    private javax.swing.JSeparator jSeparator2;
+    private javax.swing.JSeparator jSeparator3;
+    private javax.swing.JTable jTable1;
+    private javax.swing.JList lCandidatos;
+    private javax.swing.JList lSeleccionados;
+    private javax.swing.JTable tEvaluar;
     // End of variables declaration//GEN-END:variables
 }
